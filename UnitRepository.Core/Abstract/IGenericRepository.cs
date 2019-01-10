5b458270ddb5ccca216d52ca5711ee03e2a49a99
SixnaskUnz <sixnaskunz@outlook.com>
2019-01-10 20:49:46 +0700

    Add project files.

---
+++ UnitRepository.Core/Abstract/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitRepository.Core.Abstract
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll { get; }
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object id);
+    }
+}
---
+++ UnitRepository.Core/Abstract/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitRepository.Core.Abstract
+{
+    public interface IUnitOfWork<TDbContext> where TDbContext : DbContext
+    {
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+        IGenericRepository<TEntity> CreateRepositoryInstance<TEntity>(TDbContext dbContext) where TEntity : class;
+    }
+}
